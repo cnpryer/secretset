223b121d419bee22e92ab9734f08c4524b44390d
Chris Pryer <14341145+cnpryer@users.noreply.github.com>
2022-08-26 11:17:06 -0400

    Allow non-unique sequence in map_sequence (#1)

--- src/secretset/map.py
+++ src/secretset/map.py
@@ -3,26 +3,29 @@ from __future__ import annotations
 from datetime import datetime
 from typing import Sequence
 
-DataTypes = str | int | float | datetime
 
-
-def map_sequence(inputs: Sequence[DataTypes]) -> dict[DataTypes, int]:
+def map_sequence(
+    sequence: Sequence[str | int | float | datetime],
+) -> dict[str | int | float | datetime, int]:
     """Anonymize input data using integer ranges for unique data.
-    Example:
-    ```
-    >> inputs
-    ("A", "B", "C")
-    anonymous_data_map(inputs)
-    >> {"A": 0, "B": 1, "C": 2}
-    ```
-    Args:
-        inputs (Sequence[DataTypes]): Unique data to create
-        mapping for.
-    Returns:
-        dict[int, DataTypes]: Mapping of input to anonymous output data.
-    """
-    res = {_: i for i, _ in enumerate(inputs)}
 
-    assert len(res) == len(inputs), "Inputs and unique data size don't match."
+    Example:
+        ```py
+        >> sequence
+        ("A", "B", "C")
+        >> anonymous_data_map(sequence)
+        {"A": 0, "B": 1, "C": 2}
+        ```
+
+    Args:
+        sequence (Sequence[str | int | float | datetime]): Sequence of data to
+            create mapping for.
+
+    Returns:
+        dict[int, str | int | float | datetime]: Mapping of input to anonymous
+            output data.
+    """
+    unique_data = set(sequence)
+    res = {_: i for i, _ in enumerate(unique_data)}
 
     return res
