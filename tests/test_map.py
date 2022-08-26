06f3ff750a3ddf6dcde81defbca08b96db7faf17
Chris Pryer <14341145+cnpryer@users.noreply.github.com>
2022-08-26 11:25:27 -0400

    Update map_sequence test (#3)

--- tests/test_map.py
+++ tests/test_map.py
@@ -2,17 +2,8 @@ from secretset.map import map_sequence
 
 
 def test_map_sequence() -> None:
-    inputs = ("A", "B", "C")
-    res = map_sequence(inputs)
+    sequence = ("A", "A", "B", "C")
+    res = map_sequence(sequence)
 
-    unique_new_data = set()
-    for _ in res:
-        unique_new_data.add(res[_])
-
-    assert len(unique_new_data) == len(res) == len(inputs)
-
-    try:
-        map_sequence(("A", "A", "A"))
-        assert False
-    except Exception as e:
-        assert True, e
+    assert len(res) == len(set(sequence))
+    assert sorted(res) == sorted({"A": 0, "B": 1, "C": 2})
